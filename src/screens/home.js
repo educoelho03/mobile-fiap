629bed80c82efc5ecb0289d70c1c7d4e288a84b9
educoelho03 <eduardopaivacoelho@outlook.com>
2024-09-24 17:15:28 -0300

    camera tirando foto pela voz

--- src/screens/home.js
+++ src/screens/home.js
@@ -2,11 +2,12 @@ import Voice from '@react-native-voice/voice';
 import React, { useEffect, useRef, useState } from 'react';
 import { LogBox, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import Tts from 'react-native-tts';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Camera, useCameraDevice, useCameraPermission, useMicrophonePermission } from 'react-native-vision-camera';
 
 LogBox.ignoreLogs(["new NativeEventEmitter()"]);
 
-export default function Home({ navigation }) {
+export default function Home ({ navigation }) {
     const device = useCameraDevice("back");
     const [permission, setPermission] = useState(null);
     const cameraRef = useRef(null);
@@ -15,26 +16,29 @@ export default function Home({ navigation }) {
 
     const { hasPermission, requestPermission } = useCameraPermission();
     const { hasPermission: hasMicPermission, requestPermission: requestMicPermission } = useMicrophonePermission();
-
-    Tts.setDefaultLanguage('pt-BR');
+    
+    Tts.setDefaultLanguage('pt-BR'); 
     Tts.setDefaultRate(0.5); // Define a velocidade de fala
 
     useEffect(() => {
-        Tts.speak("Olá, bem vindo ao SmartCam, fale 'abrir camera' para acessar a câmera.")
+        Tts.speak("Olá, bem vindo ao SmartCam, Aperte no centro da tela para acessar a câmera.")
     }, [])
 
     useEffect(() => {
         (async () => {
-            const cameraStatus = await requestPermission();
-            const micStatus = await requestMicPermission();
-            if (cameraStatus && micStatus) {
-                setPermission(true);
-                handleListening();
-            } else {
+            try {
+                const cameraStatus = await requestPermission();
+                const micStatus = await requestMicPermission();
+                if (cameraStatus && micStatus) {
+                    setPermission(true);
+                } else {
+                    setPermission(false);
+                }
+            } catch (error) {
+                console.error('Erro ao solicitar permissões:', error);
                 setPermission(false);
             }
-        }
-        )();
+        })();
     }, [requestPermission, requestMicPermission]);
 
     const capturePhoto = async () => {
@@ -44,6 +48,7 @@ export default function Home({ navigation }) {
                     flash: 'off',
                 });
                 console.log(photo);
+                Tts.speak("Foto tirada com sucesso."); 
                 navigation.navigate('Results', { photoUri: photo.path });
             } catch (error) {
                 console.error('Erro ao tirar foto:', error);
@@ -56,14 +61,18 @@ export default function Home({ navigation }) {
         const text = value ?? [''];
         const recognizedText = text.join(' ').replace(',', ' ');
         console.log('Resultados do Reconhecimento de Voz:', recognizedText);
-
+        
         if (recognizedText.toLowerCase().includes('abrir câmera')) {
             setShowCamera(true);
+            Tts.speak("Você entrou na câmera, basta dizer 'tirar foto', para realizar uma captura.");
+            handleListening();
         } else if (recognizedText.toLowerCase().includes('tirar foto')) {
+            handleListening();
             capturePhoto();
+        } else {
+            Tts.speak("Comando não reconhecido.")
         }
     }
-
 
     async function handleListening() {
         try {
@@ -97,6 +106,18 @@ export default function Home({ navigation }) {
                 <Text style={styles.welcomeText}>
                     Bem-vindo ao SmartCam
                 </Text>
+
+                <TouchableOpacity 
+                    style={styles.voiceButton} 
+                    accessibilityLabel="Iniciar comando de voz"
+                    accessibilityHint="Toque para começar a usar comandos de voz"
+                    onPress={handleListening}
+                >
+                    <Icon name="mic" size={30} color="#fff" />
+                    <Text style={styles.buttonText}>
+                        {isListening ? "Gravando..." : "Iniciar reconhecimento de voz"}
+                    </Text>
+                </TouchableOpacity>
 
                 {showCamera && (
                     <>
@@ -135,13 +156,26 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginBottom: 20,
     },
+    voiceButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#ff5722',
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        borderRadius: 50,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        marginLeft: 10,
+    },
     captureButton: {
-        backgroundColor: '#fff',
-        width: 90,
-        height: 90,
-        borderRadius: 45,
-        borderWidth: 8,
-        borderColor: '#A6A6A6',
+        backgroundColor: '#fff', 
+        width: 90,               
+        height: 90,              
+        borderRadius: 45,        
+        borderWidth: 8,          
+        borderColor: '#A6A6A6',  
         position: 'absolute',
         bottom: 70,
         alignSelf: 'center'
